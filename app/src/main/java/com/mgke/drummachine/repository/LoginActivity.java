da23e2fac2f0845241b6f891f36b418140ad6d35
Timmatus <timmatusik@gmail.com>
2024-11-05 09:22:06 +0300

    ver 1.0

--- app/src/main/java/com/mgke/drummachine/repository/LoginActivity.java
+++ app/src/main/java/com/mgke/drummachine/repository/LoginActivity.java
@@ -1,19 +1,10 @@
 package com.mgke.drummachine.repository;
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Typeface;
+
 import android.os.Bundle;
-import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.firestore.FirebaseFirestore;
+
 import com.mgke.drummachine.R;
 
 public class LoginActivity extends AppCompatActivity {
--- app/src/main/java/com/mgke/drummachine/repository/RegisterActivity.java
+++ app/src/main/java/com/mgke/drummachine/repository/RegisterActivity.java
@@ -1,21 +1,16 @@
 package com.mgke.drummachine.repository;
 
-import android.content.Intent;
-import android.graphics.Typeface;
+
 import android.os.Bundle;
-import android.text.method.PasswordTransformationMethod;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import com.google.firebase.firestore.FirebaseFirestore;
+
 import com.mgke.drummachine.R;
 
-import java.util.regex.Pattern;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
